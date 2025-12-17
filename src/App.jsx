b4683d1a0e03514b0b1e72f0f5a6d24aaf16b6ab
Jesus Robles <jasasrobles02@gmail.com>
2025-12-17 18:48:48 -0500

    Agregando rutas

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,20 @@ import { Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Home from './pages/HomePage'
+import Menu from './pages/MenuPage'
+import Contacto from './pages/ContactoPage'
 import './App.css'
-
-
 
 
 function App() {
   return (
     <>
       <Header />
-      
+
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/contacto" element={<Contacto />} />
       </Routes>
 
       <Footer />
